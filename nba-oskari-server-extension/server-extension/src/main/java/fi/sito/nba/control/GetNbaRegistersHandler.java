e17fcf6c478d11f8248cadecf892b9413dd44e03
unknown <Krzysztof Kozlowski@C-BYD-004.sitois.local>
2016-03-22 12:36:05 +0100

    add new action route handlers for register searching

---
+++ nba-oskari-server-extension/server-extension/src/main/java/fi/sito/nba/control/GetNbaRegistersHandler.java
@@ -0,0 +1,83 @@
+package fi.sito.nba.control;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.swing.event.ListSelectionEvent;
+
+import org.deegree.io.DBConnectionPool;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import fi.nls.oskari.annotation.OskariActionRoute;
+import fi.nls.oskari.control.ActionDeniedException;
+import fi.nls.oskari.control.ActionException;
+import fi.nls.oskari.control.ActionParameters;
+import fi.nls.oskari.control.ActionParamsException;
+import fi.nls.oskari.control.RestActionHandler;
+import fi.nls.oskari.db.DatasourceHelper;
+import fi.nls.oskari.log.LogFactory;
+import fi.nls.oskari.log.Logger;
+import fi.nls.oskari.util.ResponseHelper;
+import fi.sito.nba.registry.services.AncientMonumentService;
+import fi.sito.nba.registry.models.AncientMonument;
+import java.sql.*;
+import com.microsoft.sqlserver.jdbc.*;
+
+@OskariActionRoute("GetNbaRegisters")
+public class GetNbaRegistersHandler extends RestActionHandler {
+
+	private static final Logger LOG = LogFactory
+			.getLogger(GetNbaRegistersHandler.class);
+
+	public void preProcess(ActionParameters params) throws ActionException {
+		// common method called for all request methods
+		LOG.info(params.getUser(), "accessing route", getName());
+	}
+
+	@Override
+	public void handleGet(ActionParameters params) throws ActionException {
+
+		JSONArray result = new JSONArray();
+
+		try {
+			HashMap<String, String> map = new HashMap<String, String>();
+			map.put("ancientMaintenance", "Muinaisjaann. hoitorekisteri"); //Ancient Monument Maintenance
+			map.put("ancientMonument", "Muinaisperintorekisteri"); //Ancient Monument
+			map.put("buildingHeritage", "Rakennusperintorekisteri"); //Building Heritage
+			map.put("rky1993", "RKY1993"); //RKY1993
+			map.put("rky2000", "RKY2000"); //RKY2000
+
+			try {
+
+				JSONObject item = new JSONObject();
+				item.put("id", "");
+				item.put("name", "Kaikki"); //All
+				result.put(item);
+
+				for (HashMap.Entry<String, String> entry : map.entrySet()) {
+					JSONObject itemX = new JSONObject();
+					itemX.put("id", entry.getKey());
+					itemX.put("name", entry.getValue());
+					result.put(itemX);
+
+				}
+			} catch (JSONException e) {
+				throw e;
+			}
+
+		} catch (Exception e) {
+			throw new ActionException("Error during getting registries");
+		}
+
+		ResponseHelper.writeResponse(params, result);
+	}
+
+}
