c8191245552a85d1dca90a3fcfd8635071d0705b
Krzysztof Kozlowski <krzysztof.kozlowski@sito.fi>
2017-01-26 10:45:56 +0100

    Fix configuration of gfi attributes for Ancient Maintenance

---
+++ content-resources/src/main/resources/flyway/nba/V1_8_update_registry_layers_config.sql
@@ -0,0 +1,2 @@
+UPDATE public.nba_registry_layers SET gfi_attributes='["objectName", "objectId", "nbaUrl", "areaCreateDate", "areaModifyDate", "areaAuthor"]' WHERE layer_id = 86;
+UPDATE public.nba_registry_layers SET gfi_attributes='["objectName", "objectId", "nbaUrl", "pointCreateDate", "pointModifyDate", "pointAuthor"]' WHERE layer_id = 88;
