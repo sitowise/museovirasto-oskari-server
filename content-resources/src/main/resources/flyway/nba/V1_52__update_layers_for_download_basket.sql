08f96b948dcd9dbcdc7cc92b34a006f3f659049a
Krzysztof Kozlowski <krzysztof.kozlowski@sito.fi>
2018-12-13 14:25:06 +0100

    configure layers for download basket

---
+++ content-resources/src/main/resources/flyway/nba/V1_52__update_layers_for_download_basket.sql
@@ -0,0 +1,7 @@
+UPDATE public.oskari_maplayer SET attributes='{"reverseXY":{"EPSG:3067":true},"downloadBasketLevel":1,"fullDetailLayer":"WFS_Latauskori:sde.SDE.Latauskori_kiinteat_mjpisteet"}' WHERE name = 'WFS_MJ_supistettu_sisalto:Kiinteat_muinaisjaannokset';
+
+UPDATE public.oskari_maplayer SET attributes='{"reverseXY":{"EPSG:3067":true},"downloadBasketLevel":1,"fullDetailLayer":"WFS_Latauskori:sde.SDE.Latauskori_kiinteat_mjalueet"}' WHERE name = 'WFS_MJ_supistettu_sisalto:Kiinteat_muinaisjaannokset_alueet';
+
+UPDATE public.oskari_maplayer SET attributes='{"reverseXY":{"EPSG:3067":true},"downloadBasketLevel":1,"fullDetailLayer":"WFS_Latauskori:sde.SDE.Latauskori_loytopaikat_mjpisteet"}' WHERE name = 'WFS_MJ_supistettu_sisalto:Loytopaikat';
+
+UPDATE public.oskari_maplayer SET attributes='{"reverseXY":{"EPSG:3067":true},"downloadBasketLevel":1,"fullDetailLayer":"WFS_Latauskori:sde.SDE.Latauskori_loytopaikat_mjalueet"}' WHERE name = 'WFS_MJ_supistettu_sisalto:Loytopaikat_alueet';
